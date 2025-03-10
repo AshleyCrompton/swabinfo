f0fc74dd0bb981c2edf055b2f0939c9ef4160485
AshleyCrompton <ashley.crompton229@gmail.com>
2025-03-10 12:49:06 +0000

    add files route

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
 const router = express.Router();
+
+const directoryPath = '../files'; // Change this to your target directory
 
 const getData = (req, res) => {
   const month = req.params.month;
@@ -28,6 +31,54 @@ const getData = (req, res) => {
   );
 };
 
+const getFilesInfo = (req, res) => {
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ error: 'Error reading directory', details: err.message });
+    }
+
+    const result = [];
+    let processedFiles = 0;
+
+    files.forEach((file) => {
+      const filePath = path.join(directoryPath, file);
+
+      fs.stat(filePath, (err, stats) => {
+        if (err) {
+          processedFiles++;
+          return console.error(`Error reading file stats for ${file}:`, err);
+        }
+
+        if (stats.isFile()) {
+          fs.readFile(filePath, 'utf8', (err, data) => {
+            processedFiles++;
+            if (err) {
+              return console.error(`Error reading file ${file}:`, err);
+            }
+
+            const numberOfRows = data.split('\n').length;
+            result.push({
+              file: file,
+              sizeInRows: numberOfRows,
+              created: stats.birthtime,
+            });
+
+            if (processedFiles === files.length) {
+              res.json(result);
+            }
+          });
+        } else {
+          processedFiles++;
+          if (processedFiles === files.length) {
+            res.json(result);
+          }
+        }
+      });
+    });
+  });
+};
 app.use(express.json());
 app.use(morgan('dev'));
 
@@ -39,5 +90,6 @@ const healthCheck = (req, res, next) => {
 
 router.route('/healthcheck').get(healthCheck);
 router.route('/getdata/:board/:month/:type').get(getData);
+router.route('/files').get(getFilesInfo);
 
 module.exports = app;
